499c296383222eecf6bf326790787461faf1edde
trannamtrung1st <trannamtrung1st@gmail.com>
2020-10-17 15:10:59 +0700

    refactor observer pattern

--- app/observer.py
+++ app/observer.py
@@ -1,13 +1,17 @@
 class Subject:
     def __init__(self):
-        self.observers = []
+        self.observers = {}
 
-    def register(self, ob: (object)):
-        self.observers.append(ob)
+    def register(self, key: str, ob: (object)):
+        if (key not in self.observers):
+            self.observers[key] = []
+        self.observers[key].append(ob)
 
-    def unregister(self, ob: (object)):
-        self.observers.remove(ob) 
+    def unregister(self, key: str, ob: (object)):
+        if (key not in self.observers):
+            self.observers[key] = []
+        self.observers[key].remove(ob)
 
-    def notify(self):
-        for ob in self.observers:
+    def notify(self, key: str):
+        for ob in self.observers[key]:
             ob(self)       
