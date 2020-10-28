bf30d7afc6e626d2da12c1ea18107376041f835f
bitum <aommarc1708@gmail.com>
2020-10-28 10:25:16 +0700

    no message

--- widgets/color_param_calibration_screen.py
+++ widgets/color_param_calibration_screen.py
@@ -1,6 +1,7 @@
 from PySide2.QtGui import *
 from PySide2.QtWidgets import *
 from PySide2.QtCore import *
+from ..app.helpers import * 
 
 from views.color_param_calibration_screen import Ui_ColorParamCalibScreen
 
@@ -16,3 +17,23 @@ class ColorParamCalibrationScreen(QWidget):
     def binding(self, backscreen: (), nextscreen: ()):
         self.ui.btnBack.clicked.connect(backscreen)
         self.ui.btnNext.clicked.connect(nextscreen)
+        self.ui.cbbDisplayType.currentIndexChanged.connect(self.cbbDisplayType_chose)
+        cam_array = get_all_camera_index(self)
+        self.ui.cbbDisplayType.clear()
+        for camera in cam_array:
+            self.ui.cbbDisplayType.addItem("Camera " + str(camera), userData=camera)
+        
+
+    def cbbDisplayType_chose(self):
+        self.replace_camera_widget()
+        index = self.ui.cbbDisplayType.currentData()
+        self.control_timer(index)
+
+    def replace_camera_widget(self): #showEvent chay khi nao? Need tim hieu here/ co the thay the bang j nua dc ko?
+        
+
+        self.image1 = ImageWidget()
+        self.label_w = self.ui.screen1.width()
+        self.label_h = self.ui.screen1.height()
+        self.imageLayout = self.ui.screen1.parentWidget().layout()     
+        self.imageLayout.replaceWidget(self.ui.screen1, self.image1)   
