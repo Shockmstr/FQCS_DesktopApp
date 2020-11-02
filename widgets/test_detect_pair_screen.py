53fee7a9947962a990b5b7558d7ec735f11c8cf0
Hoang Nguyen <popemkt1@gmail.com>
2020-11-02 11:06:23 +0700

    minor fixes

--- widgets/test_detect_pair_screen.py
+++ widgets/test_detect_pair_screen.py
@@ -1,13 +1,18 @@
 from PySide2.QtGui import *
 from PySide2.QtWidgets import *
 from PySide2.QtCore import *
+from models.detector_config import DetectorConfigSingleton, DetectorConfig
 
 from views.test_detect_pair_screen import Ui_test_detect_pair_screen
 
 class TestDetectPairScreen(QWidget):
+    CAMERA_LOADED = False
+
+
     def __init__(self, backscreen: (), nextscreen: ()):
         QWidget.__init__(self)
         self.ui = Ui_test_detect_pair_screen()
+        self.detector_cfg = DetectorConfigSingleton.get_instance().config
         self.ui.setupUi(self)
         self.binding(backscreen=backscreen, nextscreen=nextscreen)
 
@@ -15,6 +20,66 @@ class TestDetectPairScreen(QWidget):
     def binding(self, backscreen:(), nextscreen: ()):
         self.ui.btnBack.clicked.connect(backscreen)
         self.ui.btnNext.clicked.connect(nextscreen)
-    
 
+    def replace_camera_widget(self):
+        if not self.CAMERA_LOADED:
+            self.image1 = ImageWidget()
+            self.label_w = self.ui.screen1.width()
+            self.label_h = self.ui.screen1.height()
+            self.imageLayout = self.ui.screen1.parentWidget().layout()
+            self.imageLayout.replaceWidget(self.ui.screen1, self.image1)
+            self.CAMERA_LOADED = True
 
+    def process_contours(self, image):
+        frame_width, frame_height = self.detector_cfg[
+            "frame_width"], self.detector_cfg["frame_height"]
+        min_width, min_height = self.detector_cfg[
+            "min_width_per"], self.detector_cfg["min_height_per"]
+        min_width, min_height = frame_width * min_width, frame_height * min_height
+        find_contours_func = detector.get_find_contours_func_by_method(
+            self.detector_cfg["detect_method"])
+
+        # adjust thresh
+        if (self.detector_cfg["detect_method"] == "thresh"):
+            adj_bg_thresh = helper.adjust_thresh_by_brightness(
+                image, self.detector_cfg["d_cfg"]["light_adj_thresh"],
+                self.detector_cfg["d_cfg"]["bg_thresh"])
+            self.detector_cfg["d_cfg"]["adj_bg_thresh"] = adj_bg_thresh
+        elif (self.detector_cfg["detect_method"] == "range"):
+            adj_cr_to = helper.adjust_crange_by_brightness(
+                image, self.detector_cfg["d_cfg"]["light_adj_thresh"],
+                self.detector_cfg["d_cfg"]["cr_to"])
+            self.detector_cfg["d_cfg"]["adj_cr_to"] = adj_cr_to
+
+        boxes, cnts, proc = detector.find_contours_and_box(
+            image,
+            find_contours_func,
+            self.detector_cfg["d_cfg"],
+            min_width=min_width,
+            min_height=min_height)
+        pair, image, split_left, split_right, boxes = detector.detect_pair_and_size(
+            image,
+            find_contours_func,
+            self.detector_cfg["d_cfg"],
+            boxes,
+            cnts,
+            stop_condition=self.detector_cfg['stop_condition'],
+            detect_range=self.detector_cfg['detect_range'])
+
+        # output
+        unit = self.detector_cfg["length_unit"]
+        per_10px = self.detector_cfg["length_per_10px"]
+        sizes = []
+        for b in boxes:
+            rect, lH, lW, box, tl, tr, br, bl = b
+            if (per_10px is not None):
+                lH, lW = helper.calculate_length(
+                    lH, per_10px), helper.calculate_length(lW, per_10px)
+            sizes.append((lH, lW))
+            cv2.drawContours(image, [box.astype("int")], -1, (0, 255, 0), 2)
+            cv2.putText(image, f"{lW:.1f} {unit}", (tl[0], tl[1]),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.65, (255, 255, 0), 2)
+            cv2.putText(image, f"{lH:.1f} {unit}", (br[0], br[1]),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.65, (255, 255, 0), 2)
+        # cv2.imshow("Contours processed", proc)
+        return image, proc
