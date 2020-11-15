9fa99cb7161b3e6dd4e99a8744230f85d5f9e6db
trannamtrung1st <trannamtrung1st@gmail.com>
2020-11-15 09:14:09 +0700

    ref

--- app_models/auth_info.py
+++ app_models/auth_info.py
@@ -3,10 +3,7 @@ from PySide2.QtCore import QObject, Signal
 
 
 class AuthInfo(QObject):
-    new_token = Signal(dict)
-    refresh_token = Signal(dict)
-    remove_token = Signal(dict)
-    same_token = Signal(dict)
+    token_info_changed = Signal(dict)
 
     def __init__(self, parent=None):
         QObject.__init__(self, parent)
@@ -17,17 +14,8 @@ class AuthInfo(QObject):
         return self.__token_info
 
     def set_token_info(self, val: dict):
-        old_token = self.__token_info
         self.__token_info = val
-        if val == old_token:
-            self.same_token.emit(val)
-        elif val is None:
-            if old_token is not None:
-                self.remove_token.emit(old_token)
-        else:
-            if old_token is not None:
-                self.refresh_token.emit(val)
-            else: self.new_token.emit(val)
+        self.token_info_changed.emit(val)
 
     def is_logged_in(self):
         return self.__token_info is not None
--- widgets/login_screen.py
+++ widgets/login_screen.py
@@ -5,41 +5,38 @@ from views.login_screen import Ui_LoginScreen
 
 
 class LoginScreen(QWidget):
-    success = Signal(dict)
-    error = Signal(Exception)
-
     def __init__(self, identity_service: IdentityService, parent=None):
         QWidget.__init__(self, parent)
         self.__identity_service = identity_service
         self.ui = Ui_LoginScreen()
         self.ui.setupUi(self)
+        self.build()
         self.binding()
 
-    # data binding
-    def binding(self):
-        self.ui.btnLogin.clicked.connect(self.log_in)
+    def build(self):
         return
 
-    def log_in(self):
+    # binding
+    def binding(self):
+        self.ui.btnLogin.clicked.connect(self.btn_login_clicked)
+        return
+
+    # handler
+    def btn_login_clicked(self):
         username = self.ui.inpAcc.text()
         password = self.ui.inpPass.text()
         is_success, resp = self.__identity_service.log_in(username, password)
         if is_success and self.__identity_service.is_device_account(resp):
             self.__identity_service.save_token_json(resp)
             self.__identity_service.check_token()
-            self.success.emit(resp)
         else:
-            choice = self.on_logged_in_error(is_success)
-            self.error.emit(resp)
+            msg = QMessageBox()
+            msg.setIcon(QMessageBox.Information)
 
-    def on_logged_in_error(self, is_success):
-        msg = QMessageBox()
-        msg.setIcon(QMessageBox.Information)
+            if is_success is not None:
+                msg.setText("Invalid account or password")
+            else:
+                msg.setText("Something's wrong")
 
-        if is_success is not None:
-            msg.setText("Invalid account or password")
-        else:
-            msg.setText("Something's wrong")
-
-        msg.setWindowTitle("Login fail")
-        return msg.exec_()
+            msg.setWindowTitle("Login fail")
+            choice = msg.exec_()
